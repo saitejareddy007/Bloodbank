45aca8f1d625841249ad79a1f9326ef0ada46510
saitejareddy007 <tejareddy.d321@gmail.com>
2018-12-02 21:50:42 +0530

    updated ui

--- getuser.php
+++ getuser.php
@@ -3,16 +3,20 @@
 <head>
 <style>
 table {
-    width: 100%;
+    border-color:#E0EDF4;
+    border-width:0px;
+    border-style:Inset;
+    width:100%;
+    border-spacing:1px;
     border-collapse: collapse;
 }
 
 table, td, th {
-    border: 1px solid black;
-    padding: 5px;
+    border:0px solid black;
+    
 }
 
-th {text-align: left;}
+
 </style>
 </head>
 <body>
@@ -23,21 +27,26 @@ $q = $_GET['q'];
 include('database.php');
 
 $result = mysqli_query($con,"SELECT * FROM user WHERE blood_group = '".$q."'");
-
-echo "<table>
-        <tr>
-            <th>Name</th>
-            <th>Age</th>
-            <th>Sex</th>
-            <th>Phone number</th>
-        </tr>";
+$colorIndicator = True;
+echo "<table style='overflow-y:scroll;'>";
 while($row = mysqli_fetch_array($result)) {
-    echo "<tr>";
-    echo "<td>" . $row['Name'] . "</td>";
-    echo "<td>" . $row['age'] . "</td>";
-    echo "<td>" . $row['sex'] . "</td>";
-    echo "<td>" . $row['contact_no'] . "</td>";
-    echo "</tr>";
+    $colorIndicator = !$colorIndicator;
+    if($colorIndicator){
+        echo "<tr style = 'background-color:#FC5353; height:40px;color:white;'>";
+        echo "<td style='width:40%;' >" . $row['Name'] . "</td>";
+        echo "<td style='width:10%;' >" . $row['age'] . "</td>";
+        echo "<td style='width:20%;' >" . $row['sex'] . "</td>";
+        echo "<td style='width:30%;' >" . $row['contact_no'] . "</td>";
+        echo "</tr>";
+    }else{
+        echo "<tr style = 'background-color:white; height:40px;'>";
+        echo "<td style='width:40%;' >" . $row['Name'] . "</td>";
+        echo "<td style='width:10%;' >" . $row['age'] . "</td>";
+        echo "<td style='width:20%;' >" . $row['sex'] . "</td>";
+        echo "<td style='width:30%;' >" . $row['contact_no'] . "</td>";
+        echo "</tr>";
+    }
+    
 }
 echo "</table>";
 mysqli_close($con);
--- home.php
+++ home.php
@@ -10,16 +10,20 @@
 	<script type="text/javascript" src="script.js" ></script>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
-<body>
+<body style="overflow-y: scroll;">
     <style>
-    table, td, th {
-        border: 1px solid black;
-        padding: 5px;
-    }
+    table {
+    border-color:#E0EDF4;
+    border-width:0px;
+    border-style:Inset;
+    width:100%;
+    border-spacing:1px;
+    border-collapse: collapse;
+}
 
     th {text-align: center;}
 </style>
-	<div id="top" style="margin-top: 0;">
+	<div id="top" style="margin-bottom: 20px; position: relative; display: inline-block;">
 		<div class="img" ><img src="logo.jpeg" height="40px"></div>
 		<div class="Name" ><h1 style="color:white; top:10px;">Blood Bank</h1></div>
 		<div class="logoutbtn">
@@ -27,24 +31,39 @@
 				<button type="submit" style="padding: 30px;"><b>Logout</b></button>
 			</form>
 		</div>
+		<div style="float: right; height:80px;display: flex;"><p style="margin: auto; color: white;"><?php echo $_SESSION['username']; ?></p></div>
+	</div>
+	<div id="homeg" style="height: 1px;">
+	
 	</div>
 	<div id="home">
-		<h1 >Welcome <?php echo $_SESSION['username']; ?></h1>
-		<button onclick="fun()" >Search for donor</button>
-	
-		<div id="donorSearch" style="visibility: hidden;">
-			Blood Group:<select name="bloodGroup" onchange="showUser(this.value)">
-							<option value="" >Select</option>
-							<option value="O -ve" >O -ve</option>
-							<option value="O +ve" >O +ve</option>
-							<option value="A -ve" >A -ve</option>
-							<option value="A +ve" >A +ve</option>
-				            <option value="B -ve" >A -ve</option>
-							<option value="B +ve" >A +ve</option>
-							<option value="AB -ve" >A -ve</option>
-							<option value="AB +ve" >A +ve</option>
-						</select>
-			<div id="txtHint"><b>Person info will be listed here...</b></div>			
+		<div style="height: 140px; width: 100%;">
+			<h1 style="margin:0 auto; color: #FC5353;">Search for donor form here</>
+			<select  style="width: 50%; font-size: 18px; text-align: center; height:40px;padding: 5px; margin-bottom: 10px; color: red" name="bloodGroup" onchange="showUser(this.value)">
+								<option value="">Select a Blood-Group</option>
+								<option value="O -ve" >O -ve</option>
+								<option value="O +ve" >O +ve</option>
+								<option value="A -ve" >A -ve</option>
+								<option value="A +ve" >A +ve</option>
+					            <option value="B -ve" >A -ve</option>
+								<option value="B +ve" >A +ve</option>
+								<option value="AB -ve" >A -ve</option>
+								<option value="AB +ve" >A +ve</option>
+							</select>
+			<table >
+		        <tr style='color:white;background-color:#999999;'>
+		            <th style="font-size: 20px; width:40%;" >Name</th>
+		            <th style="font-size: 20px; width:10%;" >Age</th>
+		            <th style="font-size: 20px; width:20%;" >Sex</th>
+		            <th style="font-size: 20px; width:30%;" >Phone number</th>
+		        </tr>
+			</table>
+		</div>
+		<div id="donorSearch" style="visibility: hidde;">
+
+			<div id="txtHint">
+				<p><b>Donors info will be listed here...</b></p>
+			</div>
 		</div>
 	</div>
 </body>
